5b2a68092e6e693dd61aaf7c1ad3b5f54e6356cd
Shunya Koide <56504519+shunyagithub@users.noreply.github.com>
2024-06-14 09:30:19 +0900

    fix star

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -8,7 +8,10 @@ function Star() {
 
   const { contextSafe } = useGSAP(
     () => {
-      console.log("creating timeline");
+      gsap.set(".star", { scale: 0 });
+      gsap.set(".dot", { opacity: 0 });
+      gsap.set(".line", { opacity: 0 });
+
       tl.current = gsap
         .timeline({ paused: true })
         .to(".star", {
@@ -39,11 +42,18 @@ function Star() {
           },
           "-=1.3"
         )
-        .to(".star", {
-          y: () => window.innerHeight / 2 + 100,
-          duration: 1.8,
-          ease: "elastic.in(0.3, 0)",
-        })
+        .to(
+          ".star",
+          {
+            y: () => window.innerHeight / 2 + 100,
+            duration: 1.6,
+            stagger: {
+              each: 0.3,
+            },
+            ease: "elastic.in(0.3, 0)",
+          },
+          "-=0.5"
+        )
         .to(
           ".line",
           {
@@ -62,7 +72,7 @@ function Star() {
             },
             duration: 0.8,
             ease: "sine.in",
-            delay: 1.3,
+            delay: 1.1,
           },
           "<"
         )
@@ -92,7 +102,7 @@ function Star() {
     const angle = (360 / DOTS_COUNT) * index;
     return (
       <div key={index} style={{ rotate: `${angle}deg` }}>
-        <div className="dot absolute h-10 w-2 rounded bg-white opacity-0" />
+        <div className="dot absolute h-10 w-2 rounded bg-white" />
       </div>
     );
   });
@@ -100,14 +110,16 @@ function Star() {
   return (
     <div
       ref={container}
-      className="pointer-events-none absolute inset-0 z-20 m-auto flex items-center justify-center"
+      className="pointer-events-none absolute inset-0 z-20 m-auto flex items-center justify-center gap-4"
     >
-      <div className="star size-36 scale-0 bg-[url(./stars/star.png)] bg-contain bg-center bg-no-repeat" />
+      <div className="star size-36 bg-[url(./stars/star.png)] bg-contain bg-center bg-no-repeat" />
       <div className="absolute">{dots}</div>
       <div className="absolute mb-16 flex gap-2">
-        <div className="line h-1 w-2 bg-white opacity-0" />
-        <div className="line mt-8 h-1 w-2 bg-white opacity-0" />
+        <div className="line h-1 w-2 bg-white" />
+        <div className="line mt-8 h-1 w-2 bg-white" />
       </div>
+      {/* TODO: another star */}
+      {/* <div className="star size-36 bg-[url(./stars/star-line.png)] bg-contain bg-center bg-no-repeat" /> */}
     </div>
   );
 }
